fc9f6b04ab74e473d1ec0257807cc363140b468e
robinyeon <robin.yeon@gmail.com>
2022-10-06 14:25:09 +0900

    [FEAT] 기본 미들웨어 추가

--- back/app.js
+++ back/app.js
@@ -1,12 +1,27 @@
 const express = require("express");
+const dotenv = require("dotenv");
+const morgan = require("morgan");
 
 const app = express();
-const PORT = 5001;
+
+const PORT = process.env.PORT || 5001;
+
+app.use((req, res, next) => {
+  if (process.nextTick.NODE_ENV === "production") {
+    morgan("combined")(req, res, next);
+  } else {
+    morgan("dev")(req, res, next);
+  }
+});
+
+dotenv.config();
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 app.get("/", (req, res) => {
   res.send("Hello Team04");
 });
 
 app.listen(PORT, () => {
-  console.log("Server is running...");
+  console.log(`✅ Server is running on port ${PORT}`);
 });
