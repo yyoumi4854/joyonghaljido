0969187a48a695305eac19f97605911113c602ff
webcodur <webcodur@gmail.com>
2022-10-07 14:16:20 +0900

    에러 안뜨게 해둔 상태

--- front/pages/find.js
+++ front/pages/find.js
@@ -10,10 +10,10 @@ import {
 import { useState } from 'react';
 import ReactTooltip from 'react-tooltip'
 
-import seoulMap from '../dummy/seoul.json';
-import zoomMap from '../dummy/zoom.json';
+// import seoulMap from '../dummy/seoul.json';
+// import zoomMap from '../dummy/zoom.json';
 
-import Map from '../components/map';
+// import Map from '../components/map';
 import Ranking from '../components/ranking';
 import Review from '../components/review';
 import Info from '../components/info';
@@ -21,7 +21,7 @@ import Info from '../components/info';
 const Find = () => {
     return (
         <div style={{ display: 'flex' }}>
-            <Map />
+            {/* <Map /> */}
             <div style={{ width: '300px', height: '80vh', background: 'whitesmoke', marginLeft: '20px' }}>
                 <Ranking />
                 {/* <Review /> */}
