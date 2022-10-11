4817328fab6b7863e7560bf354bb3fdafc7d0466
robinyeon <robin.yeon@gmail.com>
2022-10-11 23:42:28 +0900

    [FEAT] 자치구별 dong리스트 조회 API 구현

--- back/src/routers/locationRouter.js
+++ back/src/routers/locationRouter.js
@@ -1,5 +1,7 @@
 const { Router } = require("express");
 const guModel = require("../db/schemas/gu");
+const dongModel = require("../db/schemas/dong");
+
 const locationRouter = Router();
 
 locationRouter.get("/gus", async (req, res) => {
@@ -14,3 +16,20 @@ locationRouter.get("/gus", async (req, res) => {
 });
 
 module.exports = locationRouter;
+
+locationRouter.get("/gus/:guId", async (req, res) => {
+  const { guId } = req.params;
+  const foundGu = await guModel.findOne({ _id: guId });
+
+  const dongsData = await dongModel.find({ guId });
+
+  const dongs = [];
+  dongsData.map((dong) => {
+    const dongData = { _id: dong._id, name: dong.name };
+    dongs.push(dongData);
+  });
+
+  const foundGuData = { _id: guId, name: foundGu.name, dongs };
+
+  res.status(200).json(foundGuData);
+});
