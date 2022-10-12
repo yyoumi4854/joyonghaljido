7cd91d085f57dc1eb1cb667cb0573b86ea470c4c
robinyeon <robin.yeon@gmail.com>
2022-10-12 14:17:18 +0900

    Revert "[REFACT] Dong: Service, Model 단계 리팩토링"
    This reverts commit c7ec1d3afa589ad44ed156e919b86aa311ceb14b.

--- back/src/routers/dongRouter.js
+++ back/src/routers/dongRouter.js
@@ -1,16 +1,43 @@
 const { Router } = require("express");
-const dongService = require("../services/dongService");
+const dongModel = require("../db/schemas/dong");
+const guModel = require("../db/schemas/gu");
 
 const dongRouter = Router();
 
-dongRouter.get("/:dongId", async (req, res, next) => {
-  try {
-    const { dongId } = req.params;
-    const dong = await dongService.getDongById(dongId);
-    res.status(200).json(dong);
-  } catch (error) {
-    next(error);
-  }
+dongRouter.get("/:dongId", async (req, res) => {
+  const { dongId } = req.params;
+
+  const foundDong = await dongModel.aggregate([
+    {
+      $match: {
+        _id: dongId,
+      },
+    },
+    {
+      $lookup: {
+        from: "gus",
+        localField: "guId",
+        foreignField: "_id",
+        as: "guName",
+      },
+    },
+    {
+      $set: {
+        guName: {
+          $arrayElemAt: ["$guName", 0],
+        },
+      },
+    },
+    {
+      $set: {
+        guName: "$guName.name",
+      },
+    },
+  ]);
+
+  const foundDongData = foundDong[0];
+
+  res.status(200).json(foundDongData);
 });
 
 module.exports = dongRouter;
