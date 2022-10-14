420878759718b1324b940ab7a0f06652376dbab0
유민지 <yyoumi4854@gmail.com>
2022-10-14 11:15:39 +0900

    [REFACT] index.js styled수정

--- front/styles/indexStyles.js
+++ front/styles/indexStyles.js
@@ -1,89 +1,85 @@
-import styled from "styled-components";
+import styled, {css} from "styled-components";
 
-const IndexStyle = styled.div`
-
-    font-size:22px;
-    font-family:notosans-kr;
-
-    h1{
-        position:relative;
-        text-align:center;
-        top:150px;
-        font-size: 48px;
-        color:#30C586;
-        font-weight: bold;
-    }
-    
-    h2{
-        position:relative;
-        text-align:center;
-        font-size: 28px;
-        color:#30C586;
-        font-weight: bold;
-        line-height:35px;
-    }
-
-
-    .imgBox{
-        position:relative;
-        top:200px;
-        text-align:center;
-        margin-bottom:200px;
-    }
-
-    .textBox{
-        position:relative;
-        top:200px;
-        line-height:27px;
-        text-align:center;
-        margin-bottom:200px;
-    }
-
-    .textBox2{
-        position:relative;
-        line-height:27px;
-        text-align:center;
-        margin-bottom:50px;
-    }
-
-    .textBox3{
-        position:relative;
-        line-height:27px;
-        text-align:center;
-        margin-bottom:40px;
-        margin-top:100px;
-    }
-
-    .greenBox{
-        padding-top:70px;
-        position:relative;
-        text-align:center;
-        line-height:27px;
-        background-color:#EBFAF3;
-        margin-bottom:100px;
-        padding-bottom:150px;
-    }
-
-    .graph{
-        position:relative;
-        width:1000px;
-        margin: 0 auto;
-    }
-
-    .graph2{
-        position:relative;
-        width:1000px;
-        background-color:white;
-        margin: 0 auto;
-        
-    }
-
-    .strong{
-        font-weight:bold;
-    }
-
+const inner = css`
+    max-width: 1200px;
+    margin: 0 auto;
 `;
 
+const IndexStyle = styled.div`
+    text-align: center;
+
+    .banner{
+        height: 480px;
+        background: ${({ theme }) => theme.colors.mainLight2};
+        &>div{
+            ${inner}
+            height: 100%;
+            background: url('/images/mainBg.jpg') no-repeat center;
+            background-size: contain;
+
+            .textCon{
+                padding-top: 148px;
+                p{
+                    margin-bottom: 16px;
+                    color: ${({ theme }) => theme.colors.main};
+                    font-size: 20px;
+                }
+                h2{
+                    font-size: ${({ theme }) => theme.fontSizes.titleFs};
+                    font-weight: 700;
+                    &.twoLines{line-height: 1.25;}
+                }
+            }
+
+            button{
+                width: 200px;
+                height: 32px;
+                margin-top: 60px;
+                background: ${({ theme }) => theme.colors.main};
+                border-radius: 32px;
+                color: #fff;
+            }
+        }
+    }
+
+    section{
+        padding: 120px 0;
+        h2{
+            font-size: 32px;
+            font-weight: 700;
+            color: ${({ theme }) => theme.colors.main};
+            &.twoLines{line-height: 1.25;}
+        }
+
+        .textBox{
+            padding-top: 40px;
+            line-height: 1.25;
+            span{font-weight: 700;}
+        }
+
+        .graph{
+            margin: 40px auto 0 auto;
+            width: 1000px;
+        }
+        &.dayNight{
+            background: ${({ theme }) => theme.colors.mainLight2};;
+        }
+    }
+
+    .questions{
+        h2{
+            font-size: 48px;
+            font-weight: 700;
+            color: ${({ theme }) => theme.colors.main};
+        }
+        .imgBox{
+            height: 435px;
+            margin-top: 80px;
+            background: url('/images/2.png') no-repeat center;
+            background-size: contain;
+        }
+    }
+`;
 export default IndexStyle;
 
 
