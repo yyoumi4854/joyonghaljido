5ddc2e6a4e138991f2a3bdd9d98111754a809dfe
robinyeon <robin.yeon@gmail.com>
2022-10-16 21:07:19 +0900

    [REFACT] Pin service, model 분리

--- back/src/db/models/Pin.js
+++ back/src/db/models/Pin.js
@@ -23,6 +23,68 @@ class Pin {
 
     return foundPins;
   }
+
+  static async getPinById(pinId) {
+    const foundPinArr = await PinModel.aggregate([
+      {
+        $match: {
+          _id: pinId,
+        },
+      },
+      {
+        $lookup: {
+          from: "dongs",
+          localField: "dongId",
+          foreignField: "_id",
+          as: "dongName",
+        },
+      },
+      {
+        $unwind: {
+          path: "$dongName",
+        },
+      },
+      {
+        $set: {
+          dongName: "$dongName.name",
+        },
+      },
+      {
+        $lookup: {
+          from: "gus",
+          localField: "guId",
+          foreignField: "_id",
+          as: "guName",
+        },
+      },
+      {
+        $unwind: {
+          path: "$guName",
+        },
+      },
+      {
+        $set: {
+          guName: "$guName.name",
+        },
+      },
+      {
+        $project: {
+          _id: 1,
+          name: 1,
+          guName: 1,
+          dongName: 1,
+          timeDecibels: 1,
+        },
+      },
+    ]);
+
+    const foundPin = foundPinArr[0];
+
+    if (!foundPin) {
+      throw new Error("해당 핀을 찾을 수 없습니다.");
+    }
+    return foundPin;
+  }
 }
 
 module.exports = Pin;
--- back/src/routers/pinRouter.js
+++ back/src/routers/pinRouter.js
@@ -1,71 +1,16 @@
 const { Router } = require("express");
-const pinModel = require("../db/schemas/pin");
+const pinService = require("../services/pinService");
 
 const pinRouter = Router();
 
-pinRouter.get("/:pinId", async (req, res) => {
-  const { pinId } = req.params;
-
-  const foundPin = await pinModel.aggregate([
-    {
-      $match: {
-        _id: pinId,
-      },
-    },
-    {
-      $lookup: {
-        from: "dongs",
-        localField: "dongId",
-        foreignField: "_id",
-        as: "dongName",
-      },
-    },
-    {
-      $set: {
-        dongName: {
-          $arrayElemAt: ["$dongName", 0],
-        },
-      },
-    },
-    {
-      $set: {
-        dongName: "$dongName.name",
-      },
-    },
-    {
-      $lookup: {
-        from: "gus",
-        localField: "guId",
-        foreignField: "_id",
-        as: "guName",
-      },
-    },
-    {
-      $set: {
-        guName: {
-          $arrayElemAt: ["$guName", 0],
-        },
-      },
-    },
-    {
-      $set: {
-        guName: "$guName.name",
-      },
-    },
-    {
-      $project: {
-        _id: 1,
-        name: 1,
-        guName: 1,
-        dongName: 1,
-        timeDecibels: 1,
-      },
-    },
-  ]);
-
-  const foundPinData = foundPin[0];
-
-  res.status(200).json(foundPinData);
+pinRouter.get("/:pinId", async (req, res, next) => {
+  try {
+    const { pinId } = req.params;
+    const foundPin = await pinService.getPinById(pinId);
+    res.status(200).json(foundPin);
+  } catch (error) {
+    next(error);
+  }
 });
 
 module.exports = pinRouter;
