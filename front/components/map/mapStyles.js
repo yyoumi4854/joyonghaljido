5ef7f533069cec7fa8253413fb5570b3188fe11e
유민지 <yyoumi4854@gmail.com>
2022-10-18 16:12:14 +0900

    [REFACT] map스타일 수정

--- front/components/map/mapStyles.js
+++ front/components/map/mapStyles.js
@@ -2,57 +2,33 @@ import styled from "styled-components";
 
 const MapContent = styled.div`
   position: relative;
-  .locationPath{
-    display: flex;
-    flex-direction: column;
-    justify-content : center;
-    align-items: center;
 
+  .title{
     position: absolute;
-    top : 20px;
-    right : 20px;
-    padding : 30px;
-    background-color : rgba(255,255,255,0.7);
-    border : 1px ${({ theme }) => theme.colors.grey3} solid;
+    top: 8%;
+    left: 50%;
+    transform: translateX(-50%);
+    font-weight: 700;
+    font-size: 28px;
+  }
 
-    .down{
-      color: ${({ theme }) => theme.colors.main};
-      padding : 10px 0px;
-    }
-    .path{
+  .locationPath{
+    ${({ theme }) => theme.common.flexCenter}
+    p{
+      color: ${({ theme }) => theme.colors.grey2};
+      cursor: pointer;
+
+      &:last-child{
+        color: #333;
+      }
+
       &:hover{
         color: ${({ theme }) => theme.colors.main};
       }
     }
-    span{
-      cursor: pointer;
-      font-size: 20px;
-      color : ${({ theme }) => theme.colors.grey2};
-      &:last-child{
-        font-size: 24px;
-        font-weight : 700;
-        color: black;
-      }
-    }
-  }
-  .locationText{
-    position: absolute;
-    display : inline;
-    top: 60px; // 기본82
-    left : 0;
-    right : 0;
-    // width : 100%;
-
-    text-align : center;
-    font-size: ${({ theme }) => theme.fontSizes.titleFs};
-    font-weight: 700;
-
     svg{
-      width: auto;
-      height: 36px;
-      vertical-align: top;
-      padding : 0 10px;
       color: ${({ theme }) => theme.colors.mainLight1};
+      margin: 0 8px;
     }
   }
 
