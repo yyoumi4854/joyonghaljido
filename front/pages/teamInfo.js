74b7e92a72cfb816f8b473ff310be521b2a68b0b
유민지 <yyoumi4854@gmail.com>
2022-10-19 16:29:51 +0900

    [REFAT] 팀소개 스타일 수정

--- front/pages/teamInfo.js
+++ front/pages/teamInfo.js
@@ -28,27 +28,27 @@ const TeamInfo = () => {
                                             src='images/teamInfo/red.png'
                                             alt="최은오"
                                         />
-                                        <Carousel.Caption className="slide">
-                                            <p>최은오</p>
-                                            <p>그림 그리는 FE 개발자 🎨</p>
-                                            <div clasName='tag'>
-                                                <span className="badge">Next.js</span>
-                                                <span className="badge">Redux</span>
-                                                <span className="badge">Express.js</span>
-                                                <span className="badge">Bootstrap</span>
-                                                <span className="badge">mongoDB</span>
+                                        <Carousel.Caption>
+                                            <div style={{ height: '100%', marginBottom: '40px' }}>
+                                                <p style={{ fontSize: '50px' }}>최은오</p>
+                                                <p style={{ fontSize: '30px', paddingTop: '40px' }}>그림 그리는 FE 개발자 🎨</p>
+                                                <div style={{ padding: '15px 0 40px 0', }}>
+                                                    <span className="badge">Next.js</span>
+                                                    <span className="badge">Redux</span>
+                                                    <span className="badge">Express.js</span>
+                                                    <span className="badge">Bootstrap</span>
+                                                    <span className="badge">mongoDB</span>
+                                                </div>
+                                                <div className="sub">
+                                                    <span>Email : pixel@kakao.com</span>
+                                                    <span>/</span>
+                                                    <span><a href="https://github.com/EunoChoi">GitHub</a></span>
+                                                </div>
                                             </div>
-                                            <div className="sub">
-                                                <span>Email : pixel@kakao.com</span>
-                                                <span>/</span>
-                                                <span><a href="https://github.com/EunoChoi">GitHub</a></span>
-                                            </div>
-                                            {/* <div>
-                                            </div> */}
                                         </Carousel.Caption>
                                     </Carousel.Item>
 
-                                    {/* <Carousel.Item>
+                                    <Carousel.Item>
                                         <img
                                             className="d-block w-100"
                                             src='images/teamInfo/orange.png'
@@ -72,7 +72,7 @@ const TeamInfo = () => {
                                             </div>
                                         </Carousel.Caption>
                                     </Carousel.Item>
-
+                                    
                                     <Carousel.Item>
                                         <img
                                             className="d-block w-100"
@@ -119,15 +119,15 @@ const TeamInfo = () => {
                                                 </div>
                                             </div>
                                         </Carousel.Caption>
-                                    </Carousel.Item> */}
+                                    </Carousel.Item>
                                 </Carousel>
                             </div>
 
                             <div className="right">
                                 <h3>백엔드</h3>
-                                <p>연다음봄, 채은빈</p>
+                                <p className="nameList">연다음봄, 채은빈</p>
 
-                                <Carousel style={{ width: '600px', height: '350px' }}>
+                                <Carousel className="slideCon">
                                     <Carousel.Item>
                                         <img
                                             className="d-block w-100"
@@ -179,166 +179,6 @@ const TeamInfo = () => {
                         </div>
                     </div>
                 </section>
-
-                    {/* <div className="sec center">
-                        <span className="title-2">팀원 소개</span>
-                    </div>
-                    <div className="sec left">
-                        <span className="title-3">프론트엔드</span>
-                        <span className="text">최은오 윤시준 유민지 박지연</span>
-                        <Carousel style={{ width: '600px', height: '350px' }}>
-                            <Carousel.Item>
-                                <img
-                                    className="d-block w-100"
-                                    src='images/teamInfo/red.png'
-                                    alt="최은오"
-                                />
-                                <Carousel.Caption>
-                                    <div style={{ height: '100%', marginBottom: '40px' }}>
-                                        <p style={{ fontSize: '50px' }}>최은오</p>
-                                        <p style={{ fontSize: '30px', paddingTop: '40px' }}>그림 그리는 FE 개발자 🎨</p>
-                                        <div style={{ padding: '15px 0 40px 0', }}>
-                                            <span className="badge">Next.js</span>
-                                            <span className="badge">Redux</span>
-                                            <span className="badge">Express.js</span>
-                                            <span className="badge">Bootstrap</span>
-                                            <span className="badge">mongoDB</span>
-                                        </div>
-                                        <div className="sub">
-                                            <span>Email : pixel@kakao.com</span>
-                                            <span>/</span>
-                                            <span><a href="https://github.com/EunoChoi">GitHub</a></span>
-                                        </div>
-                                    </div>
-                                </Carousel.Caption>
-                            </Carousel.Item>
-
-                            <Carousel.Item>
-                                <img
-                                    className="d-block w-100"
-                                    src='images/teamInfo/orange.png'
-                                    alt="윤시준"
-                                />
-                                <Carousel.Caption>
-                                    <div style={{ height: '100%', marginBottom: '40px' }}>
-                                        <p style={{ fontSize: '50px' }}>윤시준</p>
-                                        <p style={{ fontSize: '30px', paddingTop: '40px' }}>react… 리액트가 끝이라니!</p>
-                                        <div style={{ padding: '15px 0 40px 0', }}>
-                                            <span className="badge">React.js</span>
-                                            <span className="badge">javaScript</span>
-                                            <span className="badge">Bootstrap</span>
-                                            <span className="badge">CSS</span>
-                                        </div>
-                                        <div className="sub">
-                                            <span>Email : webcodur@gmail.com</span>
-                                            <span>/</span>
-                                            <span><a href="https://github.com/webcodur">GitHub</a></span>
-                                        </div>
-                                    </div>
-                                </Carousel.Caption>
-                            </Carousel.Item>
-
-                            <Carousel.Item>
-                                <img
-                                    className="d-block w-100"
-                                    src='images/teamInfo/yellow.png'
-                                    alt="유민지"
-                                />
-                                <Carousel.Caption>
-                                    <div style={{ height: '100%', marginBottom: '40px' }}>
-                                        <p style={{ fontSize: '50px' }}>유민지</p>
-                                        <p style={{ fontSize: '30px', paddingTop: '40px' }}>디자인이 좋은 FE 개발자 😊</p>
-                                        <div style={{ padding: '15px 0 40px 0', }}>
-                                            <span className="badge">React.js</span>
-                                            <span className="badge">javaScript</span>
-                                            <span className="badge">CSS</span>
-                                        </div>
-                                        <div className="sub">
-                                            <span>Email : yyoumi4854@gmail.com</span>
-                                            <span>/</span>
-                                            <span><a href="https://github.com/yyoumi4854">GitHub</a></span>
-                                        </div>
-                                    </div>
-                                </Carousel.Caption>
-                            </Carousel.Item>
-
-                            <Carousel.Item>
-                                <img
-                                    className="d-block w-100"
-                                    src='images/teamInfo/green.png'
-                                    alt="박지연"
-                                />
-                                <Carousel.Caption>
-                                    <div style={{ height: '100%', marginBottom: '40px' }}>
-                                        <p style={{ fontSize: '50px' }}>박지연</p>
-                                        <p style={{ fontSize: '30px', paddingTop: '40px' }}>왕왕<del>왕</del> 주니어 개발자 💪</p>
-                                        <div style={{ padding: '15px 0 40px 0', }}>
-                                            <span className="badge">React.js</span>
-                                            <span className="badge">javaScript</span>
-                                            <span className="badge">CSS</span>
-                                        </div>
-                                        <div className="sub">
-                                            <span>Email : katkrarrrr@gmail.com</span>
-                                            <span>/</span>
-                                            <span><a href="https://github.com/yeoooon">GitHub</a></span>
-                                        </div>
-                                    </div>
-                                </Carousel.Caption>
-                            </Carousel.Item>
-                        </Carousel>
-                    </div>
-                    <div className="sec right">
-                        <span className="title-3">백엔드</span>
-                        <span className="text">연다은봄 채은빈</span>
-                        <Carousel style={{ width: '600px', height: '350px' }}>
-                            <Carousel.Item>
-                                <img
-                                    className="d-block w-100"
-                                    src='images/teamInfo/blue.png'
-                                    alt="연다은봄"
-                                />
-                                <Carousel.Caption>
-                                    <div style={{ height: '100%', marginBottom: '40px' }}>
-                                        <p style={{ fontSize: '50px' }}>연다은봄</p>
-                                        <p style={{ fontSize: '30px', paddingTop: '40px' }}>강아지가 좋은 백엔드 🐶</p>
-                                        <div style={{ padding: '15px 0 40px 0', }}>
-                                            <span className="badge">Node.js</span>
-                                            <span className="badge">Express.js</span>
-                                            <span className="badge">MongoDB</span>
-                                        </div>
-                                        <div className="sub">
-                                            <span>Email : robin.yeon@gmail.com</span>
-                                            <span>/</span>
-                                            <span><a href="https://github.com/robinyeon">GitHub</a></span>
-                                        </div>
-                                    </div>
-                                </Carousel.Caption>
-                            </Carousel.Item>
-                            <Carousel.Item>
-                                <img
-                                    className="d-block w-100"
-                                    src='images/teamInfo/purple.png'
-                                    alt="채은빈"
-                                />
-                                <Carousel.Caption>
-                                    <div style={{ height: '100%', marginBottom: '40px' }}>
-                                        <p style={{ fontSize: '50px' }}>채은빈</p>
-                                        <p style={{ fontSize: '30px', paddingTop: '40px' }}>좋아서 하는 백엔드 🥰</p>
-                                        <div style={{ padding: '15px 0 40px 0', }}>
-                                            <span className="badge">Node.js</span>
-                                            <span className="badge">Express.js</span>
-                                            <span className="badge">MongoDB</span>
-                                        </div>
-                                        <div className="sub">
-                                            <span>Email : alrxltkfkd@gmail.com</span>
-                                            <span>/</span>
-                                            <span><a href="https://github.com/VelyVelyn">GitHub</a></span>
-                                        </div>
-                                    </div>
-                                </Carousel.Caption>
-                            </Carousel.Item>
-                        </Carousel>
-                    </div> */}
             </TeamInfoLayout>
             <FooterStyle>&copy; 2022 조용할지도</FooterStyle>
         </>
--- front/pages/teamInfo.style.js
+++ front/pages/teamInfo.style.js
@@ -1,4 +1,4 @@
-import styled, { css } from "styled-components";
+import styled from "styled-components";
 
 const TeamInfoLayout = styled.div`
     height: calc(100vh - 64px - 48px);
@@ -20,6 +20,7 @@ const TeamInfoLayout = styled.div`
 
     .slideSec{
         ${({ theme }) => theme.common.flexCenter};
+        justify-content: space-between;
         margin-top: 60px;
         text-align: center;
 
@@ -33,116 +34,33 @@ const TeamInfoLayout = styled.div`
             margin-top: 12px;
         }
         
-
         .slideCon{
-            width: 560px;
+            width: 580px;
             height: 360px;
             margin-top: 12px;
 
             .carousel-indicators{
-                bottom: 44px;
+                bottom: 32px;
             }
+        }
 
-            .slide{
-                /* ${({ theme }) => theme.common.flexCenter};
-                flex-direction: column;
-                justify-content: space-between;
-                width: 100%;
-                height: 100%; */
-                padding: 24px;
-                
-                p:nth-child(1){font-size: 32px;}
-                p:nth-child(2){
-                    font-size: 24px;
-                    margin-top: 20px;
-                }
-
-                div.tag{
-                    margin-top: 20px;
-                }
-            }
+        .badge{
+            padding : 5px 10px;
+            margin : 0 5px;
+            border-radius: 100px;
+            background-color : #fff;
+            color : #333;
+            font-weight: 400;
+        }
+        .sub span{
+            color : #fff;
+            padding : 0 10px;
+        }
+        .sub a{
+            color : white;
         }
     
     }
-    /* margin-top: 64px;
-
-    .title-1{
-        font-size : 60px;
-        font-weight : 700;
-        color : #30C586;
-        margin-bottom : 50px;
-    }
-    .title-2{
-        font-size : 40px;
-        font-weight : 700;
-        color : #30C586;
-    }
-    .title-2:nth-child(2){
-        padding : 40px 0;
-    }
-    .title-3{
-        font-size : 32px;
-        font-weight : 700;
-        color : #30C586;
-    }
-    .text{
-        font-size : 20px;
-        font-weight : 600;
-        padding : 20px 0;
-        color : ${({ theme }) => theme.colors.grey2};
-    }
-    .teamText{
-        font-size : 40px;
-        font-weight : 600;
-        color : ${({ theme }) => theme.colors.grey2};
-    }
-    .sec{
-        padding : 30px 150px;
-    }
-    .sec:first-child{
-        padding : 100px 0;
-    }
-    .sec:nth-child(2){
-        padding-top : 100px;
-    }
-    .sec:last-child{
-        padding-bottom : 100px;
-    }
-    .center{
-        display : flex;
-        flex-direction : column;
-        justify-content : center;
-        align-items : center; 
-    }
-    .left{
-        display : flex;
-        flex-direction : column;
-        justify-content : center;
-        align-items : start;
-    }
-    .right{
-        display : flex;
-        flex-direction : column;
-        justify-content : center;
-        align-items : end; 
-    }
-    .bg{
-        background-color : ${({ theme }) => theme.colors.mainLight2}
-    }
-    .badge{
-        padding : 5px 10px;
-        margin : 0 5px;
-        border-radius: 100px;
-        background-color : white;
-        color : grey;
-    }
-    .sub span{
-        color : white;
-        padding : 0 10px;
-    }
-    .sub a{
-        color : white;
-    } */
 `;
 
 export default TeamInfoLayout;
