8d692f9bfdbd51411392820d334323285ded113b
evelyn <alrxltkfkd@gmail.com>
2022-10-19 09:15:53 +0900

    [FEAT] POST 요청 제한 시간 수정

--- back/src/middlewares/ipLimitMiddleware.js
+++ back/src/middlewares/ipLimitMiddleware.js
@@ -1,10 +1,10 @@
 const rateLimit = require("express-rate-limit");
 
 const postRequestLimiter = rateLimit({
-  windowMs: 60 * 60 * 1000, //1hour
+  windowMs: 20 * 1000, //20seconds
   max: 1,
   handler: (req, res) => {
-    return res.status(429).send("시간 당 한 건의 리뷰만 등록할 수 있습니다.");
+    return res.status(429).send("20초 당 한 건의 리뷰만 등록할 수 있습니다.");
   },
 });
 
