56a600c9463aab92aaa78561836b392314e90efa
webcodur <webcodur@gmail.com>
2022-10-20 20:57:36 +0900

    no alert

--- front/components/review/functions/getAvg.js
+++ front/components/review/functions/getAvg.js
@@ -12,8 +12,9 @@ const getAvg = (v1, v2, v3) => {
     if(avg <= range) res = 1
     else if(range<avg && avg<= range*2) res = 2;
     else if(range*2 <avg) res = 3;
-    return res
-  }
 
-      alert('생성실패!')
-      export default getAvg
+    return res
+
+}
+
+export default getAvg
