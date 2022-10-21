d8db5bc498935b31802ae9e2f8e4ff67baaa87fc
webcodur <webcodur@gmail.com>
2022-10-22 04:30:16 +0900

    [fix] order method fix

--- front/components/review/functions/Load_Gu.js
+++ front/components/review/functions/Load_Gu.js
@@ -39,7 +39,7 @@ const Load_Gu = async (
             await axios.get(`http://localhost:5001/reviews?guId=${currentState.guId}&skip=${i}`)
                 .then(v => (setList((prev) => {
                 // return [...prev, ...v.data]
-                return [...v.data, ...prev]
+                return [...prev, ...v.data]
                 })));
             }
         }
@@ -53,7 +53,7 @@ const Load_Gu = async (
             for (let i = 1; i <= more; i++) {
             await axios.get(`http://localhost:5001/reviews?dongId=${currentState.clickSpotId}&skip=${i}`)
                 .then(v => (setList((prev) => {
-                    return [...v.data, ...prev]
+                    return [...prev, ...v.data]
                 })));
             }
         }
