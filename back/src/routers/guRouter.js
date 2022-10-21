72b92b6e96497f94ba07209a126081b9808373b3
robinyeon <robin.yeon@gmail.com>
2022-10-21 12:29:53 +0900

    [REFACT] CORS 오류관련

--- back/src/routers/guRouter.js
+++ back/src/routers/guRouter.js
@@ -7,10 +7,7 @@ guRouter.get("/:guId", async (req, res, next) => {
   try {
     const { guId } = req.params;
     const foundGuGeoJSON = await guService.getGuGeoById(guId);
-    res
-      .header("Access-Control-Allow-Origin", "*")
-      .status(200)
-      .json(foundGuGeoJSON);
+    res.status(200).json(foundGuGeoJSON);
   } catch (error) {
     next(error);
   }
