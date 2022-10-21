1d710a3e1eab7d5bc878bd90508fa8e47ad44dcc
evelyn <alrxltkfkd@gmail.com>
2022-10-21 15:49:01 +0900

    [FEAT] 한국시간으로 변경

--- back/src/db/schemas/review.js
+++ back/src/db/schemas/review.js
@@ -1,41 +1,48 @@
 const { Schema, model, Types } = require("mongoose");
+const moment = require("moment-timezone");
 
-const ReviewSchema = new Schema(
-  {
-    _id: {
-      type: String,
-      required: true,
-      default: () => String(new Types.ObjectId()),
-    },
-    guId: {
-      type: String,
-      required: true,
-    },
-    dongId: {
-      type: String,
-      required: true,
-    },
-    title: {
-      type: String,
-      required: true,
-    },
-    description: {
-      type: String,
-      required: true,
-    },
-    password: {
-      type: String,
-      required: true,
-    },
-    noiseLevel: {
-      type: Number,
-      required: true,
-    },
+const localTime = moment().tz("Asia/Seoul").format();
+console.log(localTime);
+
+const ReviewSchema = new Schema({
+  _id: {
+    type: String,
+    required: true,
+    default: () => String(new Types.ObjectId()),
   },
-  {
-    timestamps: true,
-  }
-);
+  guId: {
+    type: String,
+    required: true,
+  },
+  dongId: {
+    type: String,
+    required: true,
+  },
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  noiseLevel: {
+    type: Number,
+    required: true,
+  },
+  createdAt: {
+    type: String,
+    default: localTime,
+  },
+  updatedAt: {
+    type: String,
+    default: localTime,
+  },
+});
 
 //send json without password
 ReviewSchema.methods.toJSON = function () {
