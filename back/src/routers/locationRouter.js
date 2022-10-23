7259553d511394f87289d0ffee6951807623a895
robinyeon <robin.yeon@gmail.com>
2022-10-23 19:18:49 +0900

    [REFACT] locationRouter 정리: pinList만 내려주기 삭제

--- back/src/routers/locationRouter.js
+++ back/src/routers/locationRouter.js
@@ -1,6 +1,4 @@
 const { Router } = require("express");
-const guModel = require("../db/schemas/gu");
-const pinModel = require("../db/schemas/pin");
 const locationService = require("../services/locationService");
 
 const locationRouter = Router();
@@ -28,27 +26,6 @@ locationRouter.get("/gus/:guId/dongs", async (req, res) => {
   const { guId } = req.params;
   const dongs = await locationService.getDongsByGuId(guId);
   res.status(200).json(dongs);
-});
-
-locationRouter.get("/gus/:guId/pins", async (req, res) => {
-  const { guId } = req.params;
-  const foundGu = await guModel.findOne({ _id: guId });
-
-  const pinsData = await pinModel.find({ guId });
-  const pins = [];
-  pinsData.map((pin) => {
-    const pinData = {
-      _id: pin._id,
-      name: pin.name,
-      longitude: pin.longitude,
-      latitude: pin.latitude,
-    };
-    pins.push(pinData);
-  });
-
-  const foundGuData = { _id: guId, name: foundGu.name, pins };
-
-  res.status(200).json(foundGuData);
 });
 
 module.exports = locationRouter;
